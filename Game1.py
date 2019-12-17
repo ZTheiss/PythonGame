8e50de721ee4bfdf826958d5a9b5ebb8e6d95f53
ZTheiss <ztheiss97@gmail.com>
2019-12-16 23:01:37 -0600

    Initial Movement

---
+++ Game1.py
@@ -0,0 +1,40 @@
+import pygame
+import random
+
+pygame.init()
+
+wind = pygame.display.set_mode((500,500))
+pygame.display.set_caption("First Game")
+
+x = 50
+y = 50
+width = 40
+height = 60
+velocity = 5
+
+run = True
+while run:
+    pygame.time.delay(100)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        x -= velocity
+    if keys[pygame.K_RIGHT]:
+        x += velocity
+    if keys[pygame.K_UP]:
+        y -= velocity
+    if keys[pygame.K_DOWN]:
+        y += velocity
+
+    wind.fill((0,0,0))
+    pygame.draw.rect(wind, (255, 0, 0), (x,y,width,height))
+    pygame.display.update()
+    
+
+pygame.quit()
+
+
