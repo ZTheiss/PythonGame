77a1b8e796414fbd32b672231c5153c85d2eaf1c
ZTheiss <ztheiss97@gmail.com>
2019-12-18 17:55:43 -0600

    Arrow Model Added

--- Projectile.py
+++ Projectile.py
@@ -1,6 +1,9 @@
 
 import pygame
 
+arrowModel = pygame.image.load('GeneralSprits/arrow.png')
+arrowModel = pygame.transform.scale(arrowModel, (16,16))
+
 class Projectile(object):
     def __init__ (self,xlocation,ylocation,radius,color,facing):
         self.xlocation = xlocation
@@ -15,4 +18,4 @@ class Projectile(object):
             self.velocity = 80 * facing #facing for direction U=-2,R=1,D=2,L=-1
     
     def draw(self, wind):
-        pygame.draw.circle(wind, self.color, (self.xlocation, self.ylocation), self.radius)
+        wind.blit(arrowModel, (self.xlocation,self.ylocation))
