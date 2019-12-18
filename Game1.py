930aed40b580fb001c33c2bbbec306e2c49c00d2
ZTheiss <ztheiss97@gmail.com>
2019-12-17 22:46:31 -0600

    Add files via upload

--- Game1.py
+++ Game1.py
@@ -3,14 +3,16 @@ import random
 
 pygame.init()
 
-wind = pygame.display.set_mode((500,500))
+wind = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+
+
 pygame.display.set_caption("First Game")
 
 x = 50
 y = 50
-width = 40
-height = 60
-velocity = 5
+width = 60
+height = 70
+velocity = 15
 
 run = True
 while run:
@@ -19,8 +21,12 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+            break
     
     keys = pygame.key.get_pressed()
+    if keys[pygame.K_ESCAPE]:
+        pygame.quit()
+        break
     if keys[pygame.K_LEFT]:
         x -= velocity
     if keys[pygame.K_RIGHT]:
