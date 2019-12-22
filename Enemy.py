ef66998a218a5ecde741214874db3d6a8ced5e3d
ZTheiss <ztheiss97@gmail.com>
2019-12-21 22:51:08 -0600

    Enemy Added and arrows fixed

--- Enemy.py
+++ Enemy.py
@@ -1,30 +1,51 @@
+import pygame
 
+walkrightMovementEnemy = [pygame.image.load('EnemySprits/R1E.png'), pygame.image.load('EnemySprits/R2E.png'), pygame.image.load('EnemySprits/R3E.png'), 
+pygame.image.load('EnemySprits/R4E.png'), pygame.image.load('EnemySprits/R5E.png'),pygame.image.load('EnemySprits/R4E.png'), 
+pygame.image.load('EnemySprits/R6E.png'),pygame.image.load('EnemySprits/R7E.png'), pygame.image.load('EnemySprits/R8E.png'),
+pygame.image.load('EnemySprits/R9E.png'),pygame.image.load('EnemySprits/R10E.png'), pygame.image.load('EnemySprits/R11E.png')]
 
+walkleftMovementEnemy = [pygame.image.load('EnemySprits/L1E.png'), pygame.image.load('EnemySprits/L2E.png'), pygame.image.load('EnemySprits/L3E.png'), 
+pygame.image.load('EnemySprits/L4E.png'), pygame.image.load('EnemySprits/L5E.png'),pygame.image.load('EnemySprits/L4E.png'), 
+pygame.image.load('EnemySprits/L6E.png'),pygame.image.load('EnemySprits/L7E.png'), pygame.image.load('EnemySprits/L8E.png'),
+pygame.image.load('EnemySprits/L9E.png'),pygame.image.load('EnemySprits/L10E.png'), pygame.image.load('EnemySprits/L11E.png')]
 
+class Enemy(object):
 
-
-class Enemy(object){
-    walkrightMovementEnemy = [pygame.image.load('EnemySprits/R1E.png'), pygame.image.load('EnemySprits/R2E.png'), pygame.image.load('EnemySprits/R3E.png'), 
-    pygame.image.load('EnemySprits/R4E.png'), pygame.image.load('EnemySprits/R5E.png'),pygame.image.load('EnemySprits/R4E.png'), 
-    pygame.image.load('EnemySprits/R6E.png'),pygame.image.load('EnemySprits/R7E.png'), pygame.image.load('EnemySprits/R8E.png'),
-    pygame.image.load('EnemySprits/R9E.png'),pygame.image.load('EnemySprits/R10E.png'), pygame.image.load('EnemySprits/R11E.png')]
-
-    walkleftMovementEnemy = [pygame.image.load('EnemySprits/L1E.png'), pygame.image.load('EnemySprits/L2E.png'), pygame.image.load('EnemySprits/L3E.png'), 
-    pygame.image.load('EnemySprits/L4E.png'), pygame.image.load('EnemySprits/L5E.png'),pygame.image.load('EnemySprits/L4E.png'), 
-    pygame.image.load('EnemySprits/L6E.png'),pygame.image.load('EnemySprits/L7E.png'), pygame.image.load('EnemySprits/L8E.png'),
-    pygame.image.load('EnemySprits/L9E.png'),pygame.image.load('EnemySprits/L10E.png'), pygame.image.load('EnemySprits/L11E.png')]
-    def __init__(self, xlocation, ylocation, widthEnemy, heightEnemy, end):
+    def __init__(self, xlocation, ylocation, widthEnemy, heightEnemy, xend, yend):
         self.xlocation = xlocation #Left Right
         self.ylocation = ylocation #Up Down
         self.widthEnemy = widthEnemy
         self.heightEnemy = heightEnemy
-        self.end = end
+        self.xend = xend
+        self.yend = yend
+        self.path = [self.xlocation, self.xend, self.ylocation, self.yend]
         self.walkCount = 0
         self.vel = 3
 
     def draw(self, wind):
+        self.move()
+        if self.walkCount + 1 >= 33:
+            self.walkCount = 0
+        
+        if self.vel > 0:
+            wind.blit(walkrightMovementEnemy[self.walkCount//3], (self.xlocation, self.ylocation))
+            self.walkCount += 1
+        else:
+            wind.blit(walkleftMovementEnemy[self.walkCount//3], (self.xlocation, self.ylocation))
+            self.walkCount += 1
         pass
 
     def move(self):
-        pass
-}
+        if self.vel > 0: #moving Right
+            if self.xlocation + self.vel < self.path[1]: #Second element
+                self.xlocation += self.vel
+            else:
+                self.vel = self.vel * -1
+                self.walkCount = 0
+        else:
+            if self.xlocation - self.vel > self.path[0]:
+                self.xlocation += self.vel
+            else:
+                self.vel = self.vel * -1
+                self.walkCount = 0
--- Projectile.py
+++ Projectile.py
@@ -1,8 +1,17 @@
 
 import pygame
 
-arrowModel = pygame.image.load('GeneralSprits/arrow.png')
-arrowModel = pygame.transform.scale(arrowModel, (16,16))
+arrowLeft = pygame.image.load('GeneralSprits/arrowLeft.png')
+arrowLeft = pygame.transform.scale(arrowLeft, (16,16))
+
+arrowRight = pygame.image.load('GeneralSprits/arrowRight.png')
+arrowRight = pygame.transform.scale(arrowRight, (16,16))
+
+arrowUp = pygame.image.load('GeneralSprits/arrowUp.png')
+arrowUp = pygame.transform.scale(arrowUp, (16,16))
+
+arrowDown = pygame.image.load('GeneralSprits/arrowDown.png')
+arrowDown = pygame.transform.scale(arrowDown, (16,16))
 
 class Projectile(object):
     def __init__ (self,xlocation,ylocation,facing):
@@ -10,10 +19,27 @@ class Projectile(object):
         self.ylocation = ylocation
         self.facing = facing
 
-        if facing == 2 or facing == -2:
+        if facing == 2: #Down
+            self.ylocation += 20
+            self.xlocation -= 9
             self.velocity = 10 * facing
-        else:
+        if facing == -2: #Up
+            self.ylocation -= 20
+            self.xlocation -= 7.5
+            self.velocity = 10 * facing
+        elif facing == -1: #Left
+            self.xlocation -= 30 
+            self.velocity = 20 * facing #facing for direction U=-2,R=1,D=2,L=-1
+        elif facing == 1: #Right
+            self.xlocation += 8  
             self.velocity = 20 * facing #facing for direction U=-2,R=1,D=2,L=-1
     
     def draw(self, wind):
-        wind.blit(arrowModel, (self.xlocation,self.ylocation))
+        if self.facing == 1:
+            wind.blit(arrowRight, (self.xlocation,self.ylocation))
+        elif self.facing == -1:
+            wind.blit(arrowLeft, (self.xlocation,self.ylocation))
+        elif self.facing == 2:
+            wind.blit(arrowDown, (self.xlocation,self.ylocation))
+        elif self.facing == -2:
+            wind.blit(arrowUp, (self.xlocation,self.ylocation))
